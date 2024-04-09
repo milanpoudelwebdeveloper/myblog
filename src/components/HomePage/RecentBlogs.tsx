173c685a44ca903997b69d1e1c271abedd119ef4
Milan Poudel <milan1@xcenter.tech>
2024-04-09 19:30:17 +0545

    Add recent blog with ISR and revalidate after an hour

--- src/components/HomePage/RecentBlogs.tsx
+++ src/components/HomePage/RecentBlogs.tsx
@@ -1,18 +1,8 @@
-import { Box, Divider, Grid, Skeleton, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Divider, Grid, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import BlogCard from '../Common/BlogCard'
 
-import { useQuery } from '@tanstack/react-query'
-import { getBlogs } from '@/src/services/blog'
-
-const RecentPosts = () => {
-  const { data: blogs, isLoading } = useQuery({
-    queryKey: ['getRecentPosts'],
-    queryFn: () => getBlogs('all', true),
-    staleTime: 60000
-  })
-  const skeletonArray = Array.from({ length: 4 }, (_, index) => index)
-
+const RecentBlogs = ({ blogs }: { blogs: IBlog[] }) => {
   const headingColor = useColorModeValue('#1A1A1A', '#FFFFFF')
   return (
     <Box mt={{ base: 7, lg: 10 }}>
@@ -21,11 +11,10 @@ const RecentPosts = () => {
       </Text>
       <Divider borderColor="#6941C6" w={9} borderWidth={2} />
       <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }} gap={7} mt={7}>
-        {isLoading && skeletonArray?.map((index) => <Skeleton key={index} h={{ base: 300, xl: 340 }} />)}
         {blogs?.map((blog: IBlog) => <BlogCard card={blog} key={blog?.id} />)}
       </Grid>
     </Box>
   )
 }
 
-export default RecentPosts
+export default RecentBlogs
