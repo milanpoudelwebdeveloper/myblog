aba731b4f6bc8aaeeb79143ca3cf86a6cba37792
Milan Poudel <milan1@xcenter.tech>
2024-04-17 17:27:13 +0545

    link categories and popular blogs

--- src/components/Common/SideBar.tsx
+++ src/components/Common/SideBar.tsx
@@ -1,5 +1,5 @@
 import { Box, Divider, Flex, Skeleton, Text, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getPopularBlogs } from '@/src/services/blog'
 import { convertDate } from '@/src/utils/convertDate'
@@ -7,10 +7,13 @@ import { useCustomToast } from '@/src/hooks/useCustomToast'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { base64File } from '@constants/files'
+import { AuthContext } from '@/src/context/authContext'
+import Link from 'next/link'
 
 const CategoryComponent = dynamic(() => import('./Categories'))
 
 const SideBar = () => {
+  const { user } = useContext(AuthContext)
   const { showToast } = useCustomToast()
   const bgColor = useColorModeValue('white', '#1a1a1a')
   const headingColor = useColorModeValue('#1A1A1A', '#FFFFFF')
@@ -25,6 +28,9 @@ const SideBar = () => {
 
   if (error) {
     showToast('Error fetching popular posts', 'error')
+  }
+  const getDynamicLink = (id: string) => {
+    return user?.id ? `/blog/${id}?query=${user?.id}` : `/blog/${id}`
   }
 
   return (
@@ -45,32 +51,34 @@ const SideBar = () => {
         <Divider borderColor="#6941C6" w={12} borderWidth={2} mx="auto" mb={9} />
         {isLoading && <Skeleton h={470} borderRadius={20} mb={6} />}
         {data?.map((blog: IBlog) => (
-          <Box key={blog?.id} mb={4}>
-            <Flex alignItems="center" gap={{ base: 5, '1xl': 6 }}>
-              <Box position="relative" w={59} h={59} maxW="full" maxH="full" borderRadius="full" overflow="hidden" flexShrink={0}>
-                <Image
-                  src={blog?.coverimage}
-                  alt="popular"
-                  style={{
-                    objectFit: 'cover'
-                  }}
-                  fill
-                  placeholder="blur"
-                  blurDataURL={base64File}
-                />
-              </Box>
-              <Box>
-                <Text fontSize={{ base: 'sm', '1xl': 'md' }} fontWeight="700" mb={1} lineHeight="1.4">
-                  {blog?.title}
-                </Text>
+          <Link href={getDynamicLink(blog?.id as string)} key={blog?.id}>
+            <Box mb={4}>
+              <Flex alignItems="center" gap={{ base: 5, '1xl': 6 }}>
+                <Box position="relative" w={59} h={59} maxW="full" maxH="full" borderRadius="full" overflow="hidden" flexShrink={0}>
+                  <Image
+                    src={blog?.coverimage}
+                    alt="popular"
+                    style={{
+                      objectFit: 'cover'
+                    }}
+                    fill
+                    placeholder="blur"
+                    blurDataURL={base64File}
+                  />
+                </Box>
+                <Box>
+                  <Text fontSize={{ base: 'sm', '1xl': 'md' }} fontWeight="700" mb={1} lineHeight="1.4">
+                    {blog?.title}
+                  </Text>
 
-                <Text color={dateColor} fontSize="xs" fontWeight="300">
-                  {convertDate(blog?.createdat)}
-                </Text>
-              </Box>
-            </Flex>
-            <Divider my={3} borderColor={dividerColor} />
-          </Box>
+                  <Text color={dateColor} fontSize="xs" fontWeight="300">
+                    {convertDate(blog?.createdat)}
+                  </Text>
+                </Box>
+              </Flex>
+              <Divider my={3} borderColor={dividerColor} />
+            </Box>
+          </Link>
         ))}
       </Box>
       <CategoryComponent />
