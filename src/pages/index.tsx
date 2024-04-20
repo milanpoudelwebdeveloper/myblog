5fc586a4d3934bdab233d1313317e7e173b220a7
Milan Poudel <milan1@xcenter.tech>
2024-04-20 18:24:42 +0545

    chnages fewsss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import BlogCard from '@components/Common/BlogCard'
-import MainLayout from '@components/Common/MainLayout'
-import HeadingSeo from '@components/Common/HeadingSeo'
-import dynamic from 'next/dynamic'
 import { getBlogs, getFeaturedBlog } from '../services/blog'
 import { Box } from '@chakra-ui/react'
-
-const RecentBlogs = dynamic(() => import('../components/RecentBlogs'), {
-  ssr: true
-})
 
 export async function getStaticProps() {
   const blogs = await getBlogs()
@@ -22,7 +14,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ blogs, featuredBlog }: { blogs: IBlog[]; featuredBlog: IBlog }) {
+export default function Home() {
   return (
     <>
       <Box>Welcome</Box>
