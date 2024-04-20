2247fffed7d3fbd6c0a548c983a85b55c9ac9cc3
Milan Poudel <milan1@xcenter.tech>
2024-04-20 18:23:58 +0545

    chnages fewss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import MainLayout from '@components/Common/MainLayout'
 import HeadingSeo from '@components/Common/HeadingSeo'
 import dynamic from 'next/dynamic'
 import { getBlogs, getFeaturedBlog } from '../services/blog'
+import { Box } from '@chakra-ui/react'
 
 const RecentBlogs = dynamic(() => import('../components/RecentBlogs'), {
   ssr: true
@@ -24,15 +25,7 @@ export async function getStaticProps() {
 export default function Home({ blogs, featuredBlog }: { blogs: IBlog[]; featuredBlog: IBlog }) {
   return (
     <>
-      <HeadingSeo
-        link="https://codewithmilan.com/"
-        title="Home | Code With Milan"
-        description="Code With Milan is a blog site where you can find programming tutorials"
-      />
-      <MainLayout>
-        <BlogCard card={featuredBlog} imageHeight={300} imageLoadFast={true} />
-        <RecentBlogs blogs={blogs} />
-      </MainLayout>
+      <Box>Welcome</Box>
     </>
   )
 }
