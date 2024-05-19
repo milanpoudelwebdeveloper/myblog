6ae4dc04cdba07c953248e021a0bc1cf7b2b1966
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-05-19 21:19:54 +0545

    fix user update context issue

--- src/components/Admin/Common/MainLayout.tsx
+++ src/components/Admin/Common/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, GlobalStyle, LightMode } from '@chakra-ui/react'
 import React from 'react'
 import NavBar from './NavBar'
 import SideBar from './SideBar'
@@ -11,17 +11,20 @@ interface Props {
 const MainLayout = ({ children }: Props) => {
   return (
     <AdminProtectedRoute>
-      <Box mx="auto">
-        <NavBar />
-        <Flex>
-          <SideBar />
-          <Box flex={1} bg="#F5F7FA" h="87vh" overflowY="scroll">
-            <Box py={{ base: 5, '1xl': 8 }} px={8} maxW={{ base: 980, '1xl': 1200 }} mx="auto">
-              {children}
+      <LightMode>
+        <GlobalStyle />
+        <Box mx="auto">
+          <NavBar />
+          <Flex>
+            <SideBar />
+            <Box flex={1} bg="#F5F7FA" h="87vh" overflowY="scroll">
+              <Box py={{ base: 5, '1xl': 8 }} px={8} maxW={{ base: 980, '1xl': 1200 }} mx="auto">
+                {children}
+              </Box>
             </Box>
-          </Box>
-        </Flex>
-      </Box>
+          </Flex>
+        </Box>
+      </LightMode>
     </AdminProtectedRoute>
   )
 }
